190fa5b19f354c3d31419bde7d6d14f31a73dde1
Eridani <zhouanze@gmail.com>
2025-04-09 00:37:49 +0800

    Creat tags

---
+++ models/Tag.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const slugify = require('slugify');
+
+const Tag = sequelize.define('Tag', {
+id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+},
+name: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    unique: true
+},
+slug: {
+    type: DataTypes.STRING(40),
+    allowNull: false,
+    unique: true
+}
+}, {
+tableName: 'tags',
+timestamps: true,
+hooks: {
+    beforeValidate: (tag) => {
+    if (tag.name && (!tag.slug || tag.changed('name'))) {
+        tag.slug = slugify(tag.name, {
+        lower: true,
+        strict: true
+        });
+    }
+    }
+}
+});
+
+module.exports = Tag;
