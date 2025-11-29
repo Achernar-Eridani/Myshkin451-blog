fea2974312b41b5954d7525132597c93e0b5c2bc
Eridani <zhouanze@gmail.com>
2025-11-30 03:58:16 +0800

    test v2

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -4,7 +4,7 @@
         <div class="flex flex-col md:flex-row justify-between items-center">
           <div class="mb-4 md:mb-0">
             <p class="text-gray-600">
-              © {{ currentYear }} Myshkin451博客. 保留所有权利. 
+              © {{ currentYear }} Myshkin451博客； 保留所有权利.v2 
               <span class="text-xs">v2.0 - CI/CD Enabled!</span>
             </p>
           </div>
