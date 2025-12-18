f0652153889b725e610525297dd8c4ec6f8f149b
Eridani <zhouanze@gmail.com>
2025-12-19 02:03:34 +0800

    update MarkdownEditor to support Latex

--- client/src/components/MarkdownEditor.vue
+++ client/src/components/MarkdownEditor.vue
@@ -19,13 +19,22 @@
 import { Editor } from "@bytemd/vue-next";
 import gfm from "@bytemd/plugin-gfm";
 import highlight from "@bytemd/plugin-highlight";
+import math from "@bytemd/plugin-math";
+
 import "bytemd/dist/index.css";
+import "katex/dist/katex.css";
+
 import api from "../api";
 
 const props = defineProps({ modelValue: String, label: String });
 const emit = defineEmits(["update:modelValue"]);
 
-const plugins = [gfm(), highlight()];
+const plugins = [
+  gfm(), 
+  highlight(), 
+  math() 
+];
+
 const locale = { write: "编辑", preview: "预览", uploadImage: "上传图片" };
 
 async function handleUpload(files) {
@@ -34,10 +43,7 @@ async function handleUpload(files) {
     try {
       const fd = new FormData();
       fd.append("image", file);
-
-      // { url, path }
       const { url, path } = await api.uploadPostImage(fd);
-
       result.push({ url, alt: file.name });
     } catch (err) {
       console.error("图片上传失败", err);
